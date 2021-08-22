4ee5b21c35197ae7f98f1dcbdf58967d900caca3
William Park <l.jimenez.leon97@gmail.com>
2021-08-22 17:17:03 -0500

    veamos

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID
+  apiKey: "AIzaSyBwwbpXuMWyyWcri3KpVWIR3rQu4HkTgB8",
+  authDomain: "react-journalapp-5447e.firebaseapp.com",
+  projectId: "react-journalapp-5447e",
+  storageBucket: "react-journalapp-5447e.appspot.com",
+  messagingSenderId: "1090091779233",
+  appId: "1:1090091779233:web:ab4cdb55396ff7fbea5ce0"
 };
 
 firebase.initializeApp(firebaseConfig);
